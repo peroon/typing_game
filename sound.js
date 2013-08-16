8112adf2b701a40d8af48288736331703fd86f50
Keita Ishibashi <fromjpn@gmail.com>
2013-08-16 22:36:12 +0900

    sound

--- sound.js
+++ sound.js
@@ -1,3 +1,14 @@
+//440hz : la
+//660hz : mi
+
 function playRandomSound(){
-	//p('sound');
+
+	var freq0 = 440 + Math.random() * 220;
+	var freq1 = 660 + Math.random() * 220;
+	var sine1 = T("sin", {freq:freq0, mul:0.5});
+	var sine2 = T("sin", {freq:freq1, mul:0.5});
+
+	T("perc", {r:400}, sine1, sine2).on("ended", function() {
+	  this.pause();
+	}).bang().play();
 }
